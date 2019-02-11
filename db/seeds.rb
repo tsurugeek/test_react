93f84fbe911afbc92136bea7e6cb3f3855643fa3
Junya Kemmochi <github@tsurugeek.com>
2019-02-11 23:08:10 +0900

    add initial quiestion records

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,10 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+
+Question.create([
+  {sentence: "move to right", answer: "l"},
+  {sentence: "move to left", answer: "h"},
+  {sentence: "move to up", answer: "k"},
+  {sentence: "move to down", answer: "j"},
+])
