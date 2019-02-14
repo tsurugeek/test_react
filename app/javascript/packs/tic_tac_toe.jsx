6cee4ff27b89551e3a47d51799101385896dac82
Junya Kemmochi <github@tsurugeek.com>
2019-02-14 23:37:27 +0900

    add onlick to square

--- app/javascript/packs/tic_tac_toe.jsx
+++ app/javascript/packs/tic_tac_toe.jsx
@@ -7,7 +7,7 @@ import ReactDOM from 'react-dom'
 class Square extends React.Component {
   render() {
     return (
-      <button className="square">
+      <button className="square" onClick={() => alert('click')}>
         {this.props.value}
       </button>
     );
