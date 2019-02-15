5984af2a1bb0102b42531fb80621037e74463b4a
Junya Kemmochi <github@tsurugeek.com>
2019-02-16 02:43:27 +0900

    enable to go back to past

--- app/javascript/packs/tic_tac_toe.jsx
+++ app/javascript/packs/tic_tac_toe.jsx
@@ -56,12 +56,16 @@ class Game extends React.Component {
           squares: Array(9).fill(null),
         }
       ],
+      stepNumber: 0,
       xIsNext: true,
     }
   }
 
   handleClick(i) {
-    const history = this.state.history;
+    console.log("this.state.history.legth="+this.state.history.lenth);
+    console.log("this.state.stepNumber="+this.state.stepNumber);
+    const history = this.state.history.slice(0, this.state.stepNumber + 1);
+    console.log("history.legth="+history.lenth);
     const current = history[history.length - 1];
     const squares = current.squares.slice();
     if(calculateWinner(squares) || squares[i]) {
@@ -70,14 +74,31 @@ class Game extends React.Component {
     squares[i] = this.state.xIsNext ? 'X' : 'O';
     this.setState({
       history: history.concat([{squares: squares}]),
+      stepNumber: history.length,
       xIsNext: !this.state.xIsNext,
     });
   }
 
+  jumpTo(step) {
+    this.setState({
+      stepNumber: step,
+      xIsNext: (step % 2) === 0,
+    });
+  }
+
   render() {
+    console.log("Game#render");
+    console.log("this.state.history.legth="+this.state.history.lenth);
+    console.log("this.state.stepNumber="+this.state.stepNumber);
     const history = this.state.history;
-    const current = history[history.length - 1];
+    const current = history[this.state.stepNumber];
     const winner = calculateWinner(current.squares);
+    const moves = history.map((step, move) => {
+      const desc = move ? "Go to move #" + move : "Go to game start";
+      return (
+        <li><button key={move} onClick={() => this.jumpTo(move)}>{desc}</button></li>
+      );
+    });
     let status;
     if (winner) {
       status = 'Winner: ' + winner;
@@ -92,7 +113,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
